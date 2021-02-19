1fd43917fa05003aeaf8780941ac8882458620d9
GSWes <wesley.hedrick@gmail.com>
2021-02-19 17:31:58 -0500

    fixed responsive formatting

--- assets/submitForm.js
+++ assets/submitForm.js
@@ -5,9 +5,11 @@ function submitForm(){
         const {first, last, email, message_area} = e.target.elements;
         const formData = {first:first.value, last:last.value, email:email.value, message:message_area.value}
         console.log(formData)
+        console.log(form)
+        form.reset()
         await axios.post('https://getform.io/f/f6d3318a-07bb-4398-8726-356fa6357d23', formData)
-        this.reset()
     })
+    
 }
 
 submitForm()
